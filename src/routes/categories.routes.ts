e22f89853a0e907196ea0e9102fc20b2a4b8b172
Davi Costa <daviita1@hotmail.com>
2021-03-18 17:34:32 -0300

    categoriesRoutes.get add

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { response, Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
@@ -13,6 +13,12 @@ categoriesRoutes.post("/", (request, response) => {
 
     return response.status(201).send();
 
+})
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+
+    return response.json(all);
 })
 
 export { categoriesRoutes };
