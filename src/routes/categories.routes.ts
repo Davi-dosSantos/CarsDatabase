f19f55e7586a8db93b3a0e4447c6565900e4562a
Davi Costa <daviita1@hotmail.com>
2021-04-04 21:58:06 -0300

    repair categories.routes replace PostgresCategoriesRepository this roue

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,11 @@
 import { response, Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { PostgresCategoriesRepository } from "../modules/cars/repositories/PostgressCategoriesRepository";
 import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 
-const categoriesRepository = new PostgresCategoriesRepository();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description, } = request.body;
